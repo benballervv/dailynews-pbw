a3771e8746a0162605f30379266349856a0e4977
benballervv <111202415785@mhs.dinus.ac.id>
2026-01-09 00:23:44 +0700

    koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Asia/Jakarta');
 $servername = "sql100.infinityfree.com";
 $username = "if0_40858365";
 $password = "7nDjyAsd6H"; 
-$db = "if0_40858365_webdailyjournal"; // UPDATE SESUAI GAMBAR TERBARU
+$db = "if0_40858365_webdailyjournal";
 
 $conn = mysqli_connect($servername, $username, $password, $db);
 
@@ -12,3 +12,4 @@ if($conn->connect_error){
     die("connect failed : ". $conn->connect_error);
 }
 ?>
+
